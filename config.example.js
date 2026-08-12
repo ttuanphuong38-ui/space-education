46a6b8cad1731ffb889595dfe04b40c14461ec08
ttuanphuong38-ui <ttuanphuong38@gmail.com>
2026-08-12 07:42:49 +0000

    Add firebase configuration example for project setup

---
+++ config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
